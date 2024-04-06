9f7e858a81f742c0a55478587afda28ed6bd8e3c
DK <166199372+dks-web@users.noreply.github.com>
2024-04-06 13:14:27 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,7 +1,7 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
+$apiLink = "https://animeapi-5oes.onrender.com"; // add the api here without the '/' at the end!
 if (date("d") > 15){
     $apiLink = ""; // same goes for here
 }
